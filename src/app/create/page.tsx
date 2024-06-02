aa002f3f9c7132ad1c52d27ac307304f843ee9de
Eliseo Flores <eliseo_flores530@yahoo.com>
2024-06-02 14:32:20 -0700

    changed create page league selection to move league state in the url, also minor styling change so pagination buttons stay at bottom of page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import type { UseQueryResult } from '@tanstack/react-query'
 import GamesList from '../_components/games-list'
-
+import { useSearchParams, useRouter } from 'next/navigation'
 type EventTypes = {
   items: [
     game: {
@@ -14,29 +14,42 @@ type EventTypes = {
 }
 const CreatePool = () => {
   // TODO: UPDATE DATE SO THAT GAMES THAT HAVE STARTED ARE NOT INCLUDED IN THE LIST
+  const searchParams = useSearchParams()
+  const router = useRouter()
   const [page, setPage] = useState(1)
   const today = new Date()
   const year = today.getFullYear()
   const month = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1
   const day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate()
   const date = `${year}${month}${day}`
-  const [league, setLeague] = useState<'nfl' | 'nba'>('nba')
+
+  const league = searchParams.get('league')
   const { data }: UseQueryResult<EventTypes> = useQuery(['events', league, page], async () => {
     const sport = league === 'nfl' ? 'football' : 'basketball'
-    const response = await fetch(`https://sports.core.api.espn.com/v2/sports/${sport}/leagues/${league}/events?dates=${date}-20240623&page=${page}`)
+    const response = await fetch(`https://sports.core.api.espn.com/v2/sports/${sport}/leagues/${league}/events?dates=${date}-20241230&page=${page}`)
     return response.json()
   }, {
     enabled: !!league,
   }
   )
-
+  const handleOptionClick = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === '') return
+    router.push(`/create?league=${e.target.value}`)
+  }
   return (
-    <div className='page gap-8'>
+    <div className='page gap-8 min-h-screen justify-between'>
       <div className="flex flex-col">
         <label>Choose League:</label>
-        <select id='league' name='league' onChange={(event) => setLeague(event?.target.value as 'nfl' | 'nba')}>
-          <option value='nba'>NBA</option>
-          <option value='nfl'>NFL</option>
+        <select id='league' name='league' onChange={(e) => handleOptionClick(e)} defaultValue=''>
+          <option value='' disabled>
+            Please select a league
+          </option>
+          <option value='nfl'>
+            NFL
+          </option>
+          <option value='nba'>
+            NBA
+          </option>
         </select>
       </div>
       {
